b1c2c82699388107bcf6b8c117d118937772f163
Milton <miltonr2069@gmail.com>
2021-10-20 19:13:27 -0400

    Correcciones segundo intento

--- src/main/java/com/misiontic/ciclo3/reto3/model/Cabin.java
+++ src/main/java/com/misiontic/ciclo3/reto3/model/Cabin.java
@@ -16,9 +16,9 @@ public class Cabin implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String name;
     private String brand;
     private Integer rooms;
-    private String name;
     private String description;
 
     @ManyToOne
